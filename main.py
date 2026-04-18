9d24071b021365bf9a7c8b801917ece4d3076d9d
Lakshmi Devi <24881A0528@student.vardhaman.org>
2026-04-18 22:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 # Load the data
 movies = pd.read_csv('movie.csv')
-ratings = pd.read_csv('ratings.csv') 
+
+# Add compression='gzip' so Python knows how to unzip it
+ratings = pd.read_csv('ratings.csv', compression='gzip') 
 
 # Merge and print the results
-df = pd.merge(ratings, movie, on='movieId')
+df = pd.merge(ratings, movies, on='movieId')
 print("--- SUCCESS: DATA LOADED ---")
 print(df.head())
