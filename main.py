ac8164d24714916119215140bf0b71d9f4169cf0
Lakshmi Devi <24881A0528@student.vardhaman.org>
2026-04-18 22:07:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Load the data
-movies = pd.read_csv('movies.csv')
+movies = pd.read_csv('movie.csv')
 ratings = pd.read_csv('ratings.csv') 
 
 # Merge and print the results
-df = pd.merge(ratings, movies, on='movieId')
+df = pd.merge(ratings, movie, on='movieId')
 print("--- SUCCESS: DATA LOADED ---")
 print(df.head())
